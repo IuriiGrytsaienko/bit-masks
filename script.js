34ad7e956f704f8e034d9410977c6cd7fd66a9aa
Iurii Grytsaienko <iuriigrytsaenko@gmail.com>
2016-10-28 15:47:09 +0300

    delete unused script, change bHasAge and bHasNumberOfChildren functions

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var people = [1, 3, 7, 10, 11, 31, 100, 981, 1000, 1370, 1668, 1917, 1924, 4074]
 const MAN = 1;
 const WEDDING = 2;
 const HIGHER_EDUCATION = 4;
+const QUANTITY_OF_CHILDRENS = 56;
+const YEARS = 4032;
 
 const bIsMan = obj => obj & MAN;
 const bIsMarried = obj => obj & WEDDING;
 const bHasHigherEducation = obj => obj & HIGHER_EDUCATION;
-const bHasNumberOfChildren = (obj, oper, numOfChildren) => eval(parseInt(decToBin((obj >> 3), 3), 2) +  oper + numOfChildren)
-var bHasAge = (obj, oper, age) => eval(parseInt(decToBin((obj >> 6), 6), 2)+  oper + age);
+const bHasNumberOfChildren = (obj, oper, numOfChildren) => eval(((obj & QUANTITY_OF_CHILDRENS) >> 3) +  oper + numOfChildren)
+const bHasAge = (obj, oper, age) => eval( ((obj & YEARS) >> 6)+  oper + age);
 
 //man, unmarried, has higher education, under 30 years
 let somePerson4 = people.filter( item => bIsMan(item))
@@ -32,10 +34,3 @@ console.log(people);
 console.log(somePerson4);
 console.log(somePerson5);
 console.log(somePerson6);
-
-function decToBin(dec,length){
-    var out = "";
-    while(length--)
-        out += (dec >> length ) & 1;
-    return out;
-}
